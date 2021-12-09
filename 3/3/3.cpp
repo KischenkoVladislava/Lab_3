0f0e4f76688a9e6355f670fcc0bae745eaa577eb
KischenkoVladislava <92689813+KischenkoVladislava@users.noreply.github.com>
2021-12-10 01:36:53 +0300

    Update 3.cpp

--- 3/3/3.cpp
+++ 3/3/3.cpp
@@ -1,11 +1,17 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    int n, m, k, n1, r;
+    int n, m, k, r;
     cout << "Введите размер массива" << endl;
     cin >> n >> m;
+    while ((n <= 0) && (m <= 0))
+    {
+        cout << "Число элементов массива не может быть раным нулю или меньше" << endl;
+        cout << "Введите число элементов массива еще раз" << endl;
+        cin >> n >> m;
+    }
     int** a = new int* [n];
     for (int i = 0; i < n; ++i)
     {
@@ -19,7 +25,7 @@ int main()
             cin >> a[i][j];
         }
     }
-    cout << "Массив" << endl;
+    cout << "Массив:" << endl;
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
@@ -36,10 +42,13 @@ int main()
         {
             r = a[j][0];
             for (int l = 0; l < m - 1; l++)
-            a[j][l] = a[j][l + 1];
+            {
+                a[j][l] = a[j][l + 1];
+            }
             a[j][m - 1] = r;
         }
     }
+    cout << "Массив с двигом на " << k << " влево: " << endl;
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
